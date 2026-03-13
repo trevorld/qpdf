5feb713b062d739318b2823939d37e424fb5e532
Trevor L Davis <trevor.l.davis@gmail.com>
2026-03-13 10:26:34 -0700

    tests: In many file combine test use a blank pdf
    This seems to be an order faster and produces an order
    smaller temporary output file than using `pdf-example-password.original.pdf`

--- tests/testthat/test-combine.R
+++ tests/testthat/test-combine.R
@@ -7,10 +7,14 @@ test_that("pdf_combine works with many files (no 'too many open files' error)", 
   pdf_combine(rep_len(pdf_file, 2), outfile_2, password = "test")
   expect_equal(pdf_length(outfile_2, password = "test"), 2 * pages_per_file)
 
-  skip("May take a few minutes and creates a 455Mb temporary file")
+  skip("May take 40+ seconds and create a 31Mb temporary file")
   n <- 10000L
+  blank_file <- tempfile(fileext = ".pdf")
   outfile_n <- tempfile(fileext = ".pdf")
-  on.exit(unlink(outfile_n), add = TRUE)
-  pdf_combine(rep_len(pdf_file, n), outfile_n, password = "test")
-  expect_equal(pdf_length(outfile_n, password = "test"), n * pages_per_file)
+  on.exit(unlink(c(blank_file, outfile_n)), add = TRUE)
+  grDevices::pdf(blank_file)
+  grid::grid.newpage()
+  invisible(grDevices::dev.off())
+  pdf_combine(rep_len(blank_file, n), outfile_n)
+  expect_equal(pdf_length(outfile_n), n)
 })
